6308a3c50b44dafc7ae808fdcc5ebc6506bb970c
kjsik11 <kjsik11@naver.com>
2020-12-03 13:54:45 +0900

    styling uploadForm

--- components/UploadForm.tsx
+++ components/UploadForm.tsx
@@ -4,12 +4,28 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { useRouter } from 'next/router';
 const Root = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 30px;
   #file {
     display: none;
   }
   .filebox {
+    width: 100%;
     margin: 10px 0;
     height: 30px;
+  }
+  .top-buttonBox {
+    display: flex;
+    margin: 5px 0;
+  }
+  .bottom-buttonBox {
+    text-align: right;
+  }
+  .selectButton {
+    text-align: right;
   }
 `;
 
@@ -74,124 +90,126 @@ const UploadForm: React.FC<Props> = ({ className, ...props }) => {
 
   return (
     <Root className={className} {...props}>
-      <input
-        ref={refFileInput}
-        type="file"
-        id="file"
-        onChange={(e) => {
-          // setName(e.target.name);
-          // setSelectFile(e.target.files[0]);
-          // console.log(e.target.name);
-          if (e.target.files) setMyFile(e.target.files[0]);
-        }}
-      />
+      <div>
+        <input
+          ref={refFileInput}
+          type="file"
+          id="file"
+          onChange={(e) => {
+            // setName(e.target.name);
+            // setSelectFile(e.target.files[0]);
+            // console.log(e.target.name);
+            if (e.target.files) setMyFile(e.target.files[0]);
+          }}
+        />
 
-      <div>
-        <TextField
-          id="standard-basic"
-          label="Title"
-          value={artwork.title}
-          onChange={(e) => {
-            setArtwork({
-              ...artwork,
-              title: e.target.value,
-            });
-          }}
-        />
-      </div>
-      <div>
-        <TextField
-          id="standard-basic"
-          label="Name"
-          value={artwork.name}
-          onChange={(e) => {
-            setArtwork({
-              ...artwork,
-              name: e.target.value,
-            });
-          }}
-        />
-      </div>
-      <div>
-        <TextField
-          id="standard-basic"
-          label="Material"
-          value={artwork.material}
-          onChange={(e) => {
-            setArtwork({
-              ...artwork,
-              material: e.target.value,
-            });
-          }}
-        />
-      </div>
-      <div>
-        <TextField
-          id="standard-basic"
-          label="Year"
-          value={artwork.year}
-          onChange={(e) => {
-            setArtwork({
-              ...artwork,
-              year: e.target.value,
-            });
-          }}
-        />
-      </div>
-      <div>
-        <TextField
-          id="standard-basic"
-          label="width"
-          value={artwork.width}
-          onChange={(e) => {
-            setArtwork({
-              ...artwork,
-              width: e.target.value,
-            });
-          }}
-        />
-      </div>
-      <div>
-        <TextField
-          id="standard-basic"
-          label="height"
-          value={artwork.height}
-          onChange={(e) => {
-            setArtwork({
-              ...artwork,
-              height: e.target.value,
-            });
-          }}
-        />
-      </div>
-      <div className="filebox">File: {myFile && myFile.name} </div>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => refFileInput.current?.click()}
-      >
-        select file
-      </Button>
-      <div>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => {
-            handleSubmit();
-          }}
-          disabled={loading}
-        >
-          submit
-        </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => {
-            router.push('/');
-          }}
-        >
-          main
-        </Button>
+        <div>
+          <TextField
+            label="Title"
+            value={artwork.title}
+            onChange={(e) => {
+              setArtwork({
+                ...artwork,
+                title: e.target.value,
+              });
+            }}
+          />
+        </div>
+        <div>
+          <TextField
+            label="Name"
+            value={artwork.name}
+            onChange={(e) => {
+              setArtwork({
+                ...artwork,
+                name: e.target.value,
+              });
+            }}
+          />
+        </div>
+        <div>
+          <TextField
+            label="Material"
+            value={artwork.material}
+            onChange={(e) => {
+              setArtwork({
+                ...artwork,
+                material: e.target.value,
+              });
+            }}
+          />
+        </div>
+        <div>
+          <TextField
+            label="Year"
+            value={artwork.year}
+            onChange={(e) => {
+              setArtwork({
+                ...artwork,
+                year: e.target.value,
+              });
+            }}
+          />
+        </div>
+        <div>
+          <TextField
+            label="width"
+            value={artwork.width}
+            onChange={(e) => {
+              setArtwork({
+                ...artwork,
+                width: e.target.value,
+              });
+            }}
+          />
+        </div>
+        <div>
+          <TextField
+            label="height"
+            value={artwork.height}
+            onChange={(e) => {
+              setArtwork({
+                ...artwork,
+                height: e.target.value,
+              });
+            }}
+          />
+        </div>
+        <div className="filebox">File: {myFile && myFile.name} </div>
+        <div className="top-buttonBox">
+          <div className="selectButton">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => refFileInput.current?.click()}
+            >
+              select file
+            </Button>
+          </div>
+          <div className="submitButton">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                handleSubmit();
+              }}
+              disabled={loading}
+            >
+              submit
+            </Button>
+          </div>
+        </div>
+        <div className="bottom-buttonBox">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              router.push('/');
+            }}
+          >
+            main
+          </Button>
+        </div>
       </div>
     </Root>
   );
